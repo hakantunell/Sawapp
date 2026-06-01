ef10304082ef9d730f75ddbc486379cb90adb0de
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-06-01 09:23:56 +0200

    Add formatting utilities adapter

---
+++ src/format-adapter.js
@@ -0,0 +1,23 @@
+// src/format-adapter.js
+// Adapter som kopplar legacy formatteringsfunktioner till SawFormat.
+//
+// Detta är lågrisk: funktionerna är rena och påverkar bara textformatteringen.
+
+(function installFormatAdapter(global) {
+  if (!global.SawFormat) {
+    console.warn("SawFormat saknas. Formatteringsfunktioner lämnas oförändrade.");
+    return;
+  }
+
+  if (typeof global.mmToIn === "function") global.mmToInLegacy = global.mmToIn;
+  if (typeof global.fmtMm === "function") global.fmtMmLegacy = global.fmtMm;
+  if (typeof global.fmtIn === "function") global.fmtInLegacy = global.fmtIn;
+
+  global.mmToIn = global.SawFormat.mmToIn;
+  global.fmtMm = global.SawFormat.fmtMm;
+  global.fmtIn = global.SawFormat.fmtIn;
+
+  if (typeof global.update === "function") {
+    global.update();
+  }
+})(window);
