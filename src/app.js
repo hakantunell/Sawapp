0f89c4e9039143b54c3074d60a9d64c1f52e0754
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-06-01 15:54:03 +0200

    Load center block resolver adapter

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@
     "src/wane-adapter.js",
     "src/block-geometry-adapter.js",
     "src/dimension-resolver-adapter.js",
+    "src/dimension-resolver-centerblock-adapter.js",
     "src/inputs-adapter.js",
     "src/geometry-adapter.js",
     "src/dimensions-adapter.js",
