993206144cbe31c09590ea9cb893290b410868a8
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-06-02 11:25:18 +0200

    Load passive view model module

--- src/app.js
+++ src/app.js
@@ -48,6 +48,7 @@
     "src/render-timber-canvas.js",
     "src/timber-saw-list.js",
     "src/timber-saw-list-adapter.js",
+    "src/view-model.js",
     "src/dimensions-editor.js",
     "src/dimensions-editor-adapter.js",
     "src/dimensions-editor-activation-adapter.js",
