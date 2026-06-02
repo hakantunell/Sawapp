98632d7fa73efdb66dc793458876a5e8fa208d3d
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-06-02 06:59:02 +0200

    Activate activePackingDimensions from SawState

--- src/packing-dimensions-adapter.js
+++ src/packing-dimensions-adapter.js
@@ -1,8 +1,5 @@
 // src/packing-dimensions-adapter.js
 // Adapter som kopplar legacy packningshjalpare till SawPackingDimensions.
-//
-// activePackingDimensions bevaras i legacy tills dimensions-state flyttas,
-// eftersom dimensions i app.js ar en lexical variabel och inte window.dimensions.
 
 (function installPackingDimensionsAdapter(global) {
   if (!global.SawPackingDimensions) {
@@ -12,30 +9,20 @@
 
   const packing = global.SawPackingDimensions;
 
-  // Fanga alltid legacy-versionen innan vi binder om nagra funktioner.
-  const legacyActivePackingDimensions =
-    typeof global.activePackingDimensions === "function"
-      ? global.activePackingDimensions
-      : global.activePackingDimensionsLegacy;
-
-  if (legacyActivePackingDimensions) {
-    global.activePackingDimensionsLegacy = legacyActivePackingDimensions;
-    global.activePackingDimensions = legacyActivePackingDimensions;
+  if (typeof global.activePackingDimensions === "function") {
+    global.activePackingDimensionsLegacy = global.activePackingDimensions;
   }
-
   if (typeof global.circleWidthAtY === "function") {
     global.circleWidthAtYLegacy = global.circleWidthAtY;
   }
-
   if (typeof global.dimensionToPackCandidate === "function") {
     global.dimensionToPackCandidateLegacy = global.dimensionToPackCandidate;
   }
-
   if (typeof global.rectFitsCircle === "function") {
     global.rectFitsCircleLegacy = global.rectFitsCircle;
   }
 
-  // Dessa ar rena hjalpmetoder och kan bindas till modulen direkt.
+  global.activePackingDimensions = packing.activePackingDimensionsFromGlobal;
   global.circleWidthAtY = packing.circleWidthAtY;
   global.dimensionToPackCandidate = packing.dimensionToPackCandidate;
   global.rectFitsCircle = packing.rectFitsCircle;
